51a29cc5ea908908a65815381617cfc75c74fbf4
mehtabhussain12 <mehtabmughal688@gmail.com>
2025-07-10 12:56:38 +0500

    complete ui

--- index.js
+++ index.js
@@ -11,109 +11,129 @@ let Questions = [
         option: ["Himalayas", "Karakoram", "Hindu Kush", "All of these"],
         correctanswer: "All of these"
     },
-    // More questions follow...
-    {
-        id: 3,
-        Question: "Pakistan's national language is?",
-        option: ["Punjabi", "Urdu", "Sindhi", "English"],
-        correctanswer: "Urdu"
-    },
-    {
-        id: 4,
-        Question: "When did Pakistan win its first Olympic gold medal?",
-        option: ["1960", "1964", "1968", "1972"],
-        correctanswer: "1960"
-    },
-    {
-        id: 5,
-        Question: "The Lahore Resolution was passed in which year?",
-        option: ["1940", "1947", "1935", "1956"],
-        correctanswer: "1940"
-    },
-    {
-        id: 6,
-        Question: "Which is the largest province of Pakistan by area?",
-        option: ["Punjab", "Sindh", "Khyber Pakhtunkhwa", "Balochistan"],
-        correctanswer: "Balochistan"
-    },
-    {
-        id: 7,
-        Question: "Pakistan's national flower is?",
-        option: ["Rose", "Jasmine", "Tulip", "Sunflower"],
-        correctanswer: "Jasmine"
-    },
-    {
-        id: 8,
-        Question: "The K2 mountain is located in which range?",
-        option: ["Himalayas", "Karakoram", "Hindu Kush", "Pamir"],
-        correctanswer: "Karakoram"
-    },
-    {
-        id: 9,
-        Question: "Which river is known as the lifeline of Pakistan?",
-        option: ["Jhelum", "Chenab", "Indus", "Ravi"],
-        correctanswer: "Indus"
-    },
-    {
-        id: 10,
-        Question: "Pakistan's first Governor-General was?",
-        option: ["Liaquat Ali Khan", "Quaid-e-Azam", "Ayub Khan", "Iskander Mirza"],
-        correctanswer: "Quaid-e-Azam"
-    },
+    // {
+    //     id: 3,
+    //     Question: "Pakistan's national language is?",
+    //     option: ["Punjabi", "Urdu", "Sindhi", "English"],
+    //     correctanswer: "Urdu"
+    // },
+    // {
+    //     id: 4,
+    //     Question: "When did Pakistan win its first Olympic gold medal?",
+    //     option: ["1960", "1964", "1968", "1972"],
+    //     correctanswer: "1960"
+    // },
+    // {
+    //     id: 5,
+    //     Question: "The Lahore Resolution was passed in which year?",
+    //     option: ["1940", "1947", "1935", "1956"],
+    //     correctanswer: "1940"
+    // },
+    // {
+    //     id: 6,
+    //     Question: "Which is the largest province of Pakistan by area?",
+    //     option: ["Punjab", "Sindh", "Khyber Pakhtunkhwa", "Balochistan"],
+    //     correctanswer: "Balochistan"
+    // },
+    // {
+    //     id: 7,
+    //     Question: "Pakistan's national flower is?",
+    //     option: ["Rose", "Jasmine", "Tulip", "Sunflower"],
+    //     correctanswer: "Jasmine"
+    // },
+    // {
+    //     id: 8,
+    //     Question: "The K2 mountain is located in which range?",
+    //     option: ["Himalayas", "Karakoram", "Hindu Kush", "Pamir"],
+    //     correctanswer: "Karakoram"
+    // },
+    // {
+    //     id: 9,
+    //     Question: "Which river is known as the lifeline of Pakistan?",
+    //     option: ["Jhelum", "Chenab", "Indus", "Ravi"],
+    //     correctanswer: "Indus"
+    // },
+    // {
+    //     id: 10,
+    //     Question: "Pakistan's first Governor-General was?",
+    //     option: ["Liaquat Ali Khan", "Quaid-e-Azam", "Ayub Khan", "Iskander Mirza"],
+    //     correctanswer: "Quaid-e-Azam"
+    // },
 ];
 let currentQuestion = 0
- let score = 0
- document.getElementById("nextbtn").disabled = false;
-//  let resetbtn =document.getElementById("resetbtn").hidden = true;
+let score = 0
+let current = document.getElementById('current')
+let result = document.getElementById("score")
+let nextbtn = document.getElementById("nextbtn")
+let resetbtn = document.getElementById("resetbtn")
+let optionElement = document.getElementById("options")
+let questionsElement = document.getElementById("questions")
+let quizContainer = document.getElementById("quiz-container")
+let resultcontainer = document.getElementById("result")
+resetbtn.style.display = "none"
 function showQuestions() {
- document.getElementById('current').textContent = currentQuestion + 1;
-    let questionsElement = document.getElementById("questions")
+    nextbtn.disabled = true
+    current.innerText = currentQuestion +1
+
     questionsElement.innerHTML = Questions[currentQuestion].Question
 
-    let optionElement = document.getElementById("options")
     optionElement.innerHTML = '';
     Questions[currentQuestion].option.map((element) => {
         optionElement.innerHTML += `<button onclick="checkAnswer(event)" class="option-btn" id="selctBtn">${element}</button>`
-        // console.log(element)
     })
-   
-    let result = document.getElementById("score")
     result.innerText = `Score: ${score}`;
-}
+} showQuestions();
+
 function checkAnswer(event) {
-    let selectedAnswer = event.target.innerText;
+
+    let selectedBtn = event.target;
+    let selectedAnswer = selectedBtn.innerText;
     let correctAnswer = Questions[currentQuestion].correctanswer;
-    if (selectedAnswer == correctAnswer) {
+    let options = document.querySelectorAll(".option-btn");
+
+    nextbtn.disabled = false;
+    options.forEach((btn) => {
+        btn.disabled = true;
+    });
+    if (selectedAnswer === correctAnswer) {
+        selectedBtn.classList.add("active");
         score++;
-        event.target.classList.add("active");
+    } else {
+        selectedBtn.classList.add("inactive");
     }
-    else {
-        event.target.classList.add("inactive");
-    }  
+
+
+    document.getElementById("score").innerText = `Score: ${score}`;
+
 }
-  
-        function NextQuestion() {
-            currentQuestion++;
-            if (currentQuestion >= Questions.length) {
-                document.getElementById("questions").innerHTML = "Quiz Completed!";
-                document.getElementById("options").innerHTML = "";
-                document.getElementById("nextbtn").disabled = true;
-                resetbtn.hidden = false;
-                  document.getElementById("questions").innerHTML = 
-                    `<h2>Quiz Completed!</h2><p>Your final score is ${score}/${Questions.length}</p>`;
-                return;
-            }
-            
-            showQuestions();
-       
-        }
-        // Reset()
-        showQuestions();
-        // function Reset(){
-        //     currentQuestion = 0;
-        //     score = 0;
-        //     document.getElementById("nextbtn").disabled = false;
-        //     showQuestions();
-        //     let result = document.getElementById("score");
-        //     result.innerText = score;
-        // }
+
+
+function NextQuestion() {
+    if (currentQuestion +1 >= Questions.length) {
+        nextbtn.disabled = true;
+        nextbtn.style.display = "none"
+        resetbtn.style.display = "block"
+        quizContainer.style.display = "none"
+        resultcontainer.style.display="block"
+        resultcontainer.innerHTML =
+            `<h2>Quiz Completed!</h2><p>Your final score is ${score}/${Questions.length}</p>`;
+        return; 
+    }
+
+    currentQuestion++;
+    showQuestions();
+}
+
+function Reset() {
+     currentQuestion = 0
+    showQuestions()
+    resultcontainer.style.display = "none"
+    quizContainer.style.display = "block"
+   
+    score = 0
+    
+    nextbtn.style.display = "block"
+    nextbtn.disabled = false
+    resetbtn.style.display = "none"
+   
+}
